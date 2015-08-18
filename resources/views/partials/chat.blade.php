f3468042fe93bd03cb50fa0152bf403a7a200f2c
Eric Tendian <erictendian@gmail.com>
2015-08-18 11:13:02 -0400

    Adding notification sound for each chat message received

--- resources/views/partials/chat.blade.php
+++ resources/views/partials/chat.blade.php
@@ -1,5 +1,6 @@
 <div id="firechat-wrapper"></div>
 <script type="text/javascript">
+  var audio = new Audio('notification.mp3');
   var chatRef = new Firebase("https://ysfhq-chat.firebaseio.com");
   var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
   chatRef.onAuth(function(authData) {
@@ -12,5 +13,8 @@
         }
       });
     }
+  });
+  chat.on('message-add', function () {
+    audio.play();
   });
 </script>
