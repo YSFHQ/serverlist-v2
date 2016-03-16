16871972fd34ef37069bc1f1bb86f6683af7e71c
Eric Tendian <erictendian@gmail.com>
2016-03-15 20:10:25 -0500

    Adding global CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Barryvdh\Cors\HandleCors::class,
         \Fideloper\Proxy\TrustProxies::class,
         //\YSFHQ\Http\Middleware\VerifyCsrfToken::class,
         \YSFHQ\Http\Middleware\ValidProxies::class,
