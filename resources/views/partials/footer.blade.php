4f0da2ea2447665b7a7c834b29af0248b348b913
Eric Tendian <erictendian@gmail.com>
2016-06-04 12:17:59 -0500

    Making footer date variable, updating Bootstrap

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="footer">
     <div class="container">
         <p class="text-muted credit">
-            &copy; 2015 <a href="http://ysfhq.com/">YSFlight Headquarters</a>
+            &copy; {{ date("Y") }} <a href="http://ysfhq.com/">YSFlight Headquarters</a>
         </p>
     </div>
 </div>
